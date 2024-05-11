fb12630545e983dadf8665d618676a1e0d5cb9c6
VladislavIlin7 <svladilin@gmail.com>
2024-05-11 18:53:08 +0700

    Made a working project

--- app/src/main/java/com/example/altayattractions/MainActivity.java
+++ app/src/main/java/com/example/altayattractions/MainActivity.java
@@ -1,33 +1,76 @@
 package com.example.altayattractions;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-
+import com.example.altayattractions.db.DataBase;
+import com.example.altayattractions.domain.Place;
 import com.example.altayattractions.service.MapService;
-import com.example.altayattractions.service.MyService;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CreatorMap {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // Создаем и запускаем новый поток для чтения данных из Firebase и их передачи в DataBase
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Получаем ссылку на узел "places" в Firebase Realtime Database
+                DatabaseReference databaseRef = FirebaseDatabase.
+                        getInstance().getReference().child("places");
+
+                databaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot placeSnapshot : dataSnapshot.getChildren()) {
+                            String name = placeSnapshot.child("name").getValue(String.class);
+                            String pathToImage = placeSnapshot.child("pathToImage").getValue(String.class);
+                            String address = placeSnapshot.child("address").getValue(String.class);
+                            String informations = placeSnapshot.child("informations").getValue(String.class);
+                            double latitude = placeSnapshot.child("latitude").getValue(Double.class);
+                            double longitude = placeSnapshot.child("longitude").getValue(Double.class);
+
+
+                            DataBase.add(new Place(name, pathToImage, address, informations, latitude, longitude));
+                        }
+
+                        // Передаем список в основной поток после завершения чтения данных из Firebase
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                createMap();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+
+                });
+            }
+        }).start();
+    }
+
+    @Override
+    public void createMap() {
+
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, MyService.class);
-        startService(intent);
-
-
-        SupportMapFragment fragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(new MapService(this));
     }
 }
